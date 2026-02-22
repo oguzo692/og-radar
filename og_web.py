12765dad6ff16ba45f275d5dc3f021f2c0237b32
oguzo692 <jpjypzkjsw@privaterelay.appleid.com>
2026-02-22 23:51:42 +0300

    Update og_web.py

--- og_web.py
+++ og_web.py
@@ -8,9 +8,9 @@ import numpy as np
 # --- 1. AYARLAR ---
 st.set_page_config
 page_title="OG Core", 
-    page_icon="🛡️", 
-    layout="wide", 
-    initial_sidebar_state="expanded"
+page_icon="🛡️", 
+layout="wide", 
+initial_sidebar_state="expanded"
 
 # --- 2. VERİ BAĞLANTISI (GOOGLE SHEETS) ---
 def get_live_data():
