987d4caac6efcb93e20a00876835be77e695a09e
oguzo692 <jpjypzkjsw@privaterelay.appleid.com>
2026-02-22 23:51:11 +0300

    Update og_web.py

--- og_web.py
+++ og_web.py
@@ -7,7 +7,7 @@ import numpy as np
 
 # --- 1. AYARLAR ---
 st.set_page_config
-    page_title="OG Core", 
+page_title="OG Core", 
     page_icon="🛡️", 
     layout="wide", 
     initial_sidebar_state="expanded"
