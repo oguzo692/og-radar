8289f7bd985bd5cce0623a61566b583fbc4b849a
oguzo692 <jpjypzkjsw@privaterelay.appleid.com>
2026-04-23 15:22:57 +0300

    Update og_web.py

--- og_web.py
+++ og_web.py
@@ -1429,7 +1429,7 @@ if check_password():
 
     elif page == "⚽ FORMLINE":
         
-    st.markdown(
+        st.markdown(
         f"<div class='industrial-card'><div class='terminal-header'>📈 PERFORMANS</div><div class='terminal-row'><span>NET:</span><span style='color:#00ff41; font-size:32px; font-family:Orbitron;'>${toplam_bahis_kar:,.2f}</span></div></div>",
         unsafe_allow_html=True
     )
