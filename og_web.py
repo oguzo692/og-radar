d47f2dc3a9c5bd07d93673ba34d9c8951d99cce0
oguzo692 <jpjypzkjsw@privaterelay.appleid.com>
2026-04-23 15:14:59 +0300

    Update og_web.py

--- og_web.py
+++ og_web.py
@@ -1434,7 +1434,7 @@ if check_password():
         )
 
     t13, t12, t11, t10, t9, t8, t7, t6, t5, t4, t1, t2, t3 = st.tabs(["⏳ W13", "❌ W12", "❌ W11", "✅ W10", "❌ W9", "❌ W8", "✅ W7", "❌ W6", "❌ W5", "❌ W4", "✅ W3", "✅ W2", "❌ W1"])
-         with t13:
+        with t13:
             st.markdown(w13_coupon_html, unsafe_allow_html=True)
         with t12:
             st.markdown(w12_coupon_html, unsafe_allow_html=True)
